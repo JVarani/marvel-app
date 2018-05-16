8a0f87e497bef4b513bf1f535320b849c57b2442
unknown <joao@netpos.com.br>
2018-05-16 12:59:26 -0300

    Change request

--- app/src/main/java/com/example/netpos/marvelapp/MainActivity.java
+++ app/src/main/java/com/example/netpos/marvelapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.netpos.marvelapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,12 +10,18 @@ import com.example.netpos.marvelapp.model.CharacterDataWrapper;
 import com.example.netpos.marvelapp.service.APIClient;
 import com.example.netpos.marvelapp.service.IRest;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     private TextView tvText;
+    private static final String APIKEY = "a413ad442cdbeabdd779f3114ec2fa05";
+    private static final String TS = "1";
+    private static final String HASH = "28e4881685d9a9d9d9b042812217b15e";
+    private IRest iRest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +30,21 @@ public class MainActivity extends AppCompatActivity {
 
         tvText = findViewById(R.id.tv_text);
         APIClient apiClient = new APIClient();
-        IRest IRest = apiClient.getAPIClient();
+        iRest = apiClient.getAPIClient();
 
+        getCharactersRequest();
+    }
+
+    private void getCharactersRequest(){
         try {
-            Call<CharacterDataWrapper> getCharacters = IRest.getCharacters("1", "a413ad442cdbeabdd779f3114ec2fa05", "28e4881685d9a9d9d9b042812217b15e");
+            Call<CharacterDataWrapper> getCharacters = iRest.getCharacters(TS, APIKEY, HASH);
 
             getCharacters.enqueue(new Callback<CharacterDataWrapper>() {
                 @Override
-                public void onResponse(Call<CharacterDataWrapper> call, Response<CharacterDataWrapper> response) {
+                public void onResponse(@NonNull Call<CharacterDataWrapper> call, @NonNull Response<CharacterDataWrapper> response) {
                     if (response.isSuccessful()){
                         CharacterDataWrapper characterDataWrapper = response.body();
+                        assert characterDataWrapper != null;
                         tvText.setText(characterDataWrapper.getData().getResults().get(1).getName());
                     } else {
                         Log.i("FOI", ""+response.body());
@@ -40,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 @Override
-                public void onFailure(Call<CharacterDataWrapper> call, Throwable t) {
+                public void onFailure(@NonNull Call<CharacterDataWrapper> call, @NonNull Throwable t) {
                     Log.i("FOI", "Error, onFailure");
                 }
             });
